0cd15b845a16b1382de8869782dfea556234d335
Robert <Robert.Elias@live.com>
2019-12-18 14:17:16 -0800

    Created useState on App.j

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import './App.css';
 
-import React from 'react';
+import React, {useState} from 'react';
+
+import NoteForm from './Form';
 import logo from './logo.svg';
 
 function App() {
+  
+  const [notes, setNotes] = useState([
+    {
+      id: 0,
+      name: "Robert Elias",
+      email: "WebDevRob@gmail.com",
+      role: "Web Developer"
+    },
+    
+    {
+      id: 1,
+      name: "John Smith",
+      email: "JohnSmith@gmail.com",
+      role: "Product Manager"
+    }
+  ])
+  
+  const Form = props => {
+    return (
+      <div className="note-list">
+      {props.notes.map(note => (
+        <div className="note" key={note.id}>
+        <p>Name: {note.name}</p>
+        <p>Email: {note.email}</p>
+        <p>Role: {note.role}</p>
+        </div>
+      ))}
+      </div>
+    );
+  };
+  
+  const addNewNote = note => {
+    const newNote = {
+      id: Date.now(),
+      name: note.name,
+      email: note.email,
+      role: note.role
+    };
+    setNotes([...notes, newNote])
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My Team</h1>
+     <NoteForm addNewNote={addNewNote}/>
+     <Form notes={notes}/>
     </div>
   );
 }
