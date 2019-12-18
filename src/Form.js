1fbbb3c6f81df9cd94379640e233bfaf8056a820
Robert <Robert.Elias@live.com>
2019-12-18 13:45:36 -0800

    Create label and inputs for name, email, role

--- src/Form.js
+++ src/Form.js
@@ -4,10 +4,28 @@ import React, {useState} from 'react'
 
 const NoteForm = () => {
     return (
-        <div>
+        <form>
+        <label htmlFor="name">Name:</label>
+        <input
+        id="name"
+        type="text"
+        name="name"/>
+
+        <label htmlFor="email">Email:</label>
+        <input
+        id="email"
+        type="text"
+        name="email"/>
+        
+        <label htmlFor="name">Role:</label>
+        <input
+        id="role"
+        type="text"
+        name="role"/>
+
             
-        </div>
-    )
+        </form>
+    );
 }
 
 export default NoteForm
